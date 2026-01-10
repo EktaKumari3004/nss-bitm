3c2f57121c3ed8550f443c64fad770935f3f2fc7
EktaKumari3004 <ektak9341@gmail.com>
2026-01-11 00:42:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Notices from "./components/Notices";
+import AboutUs from "./components/AboutUs";
+import ActivityPage from "./components/ActivityPage";
 
 export default function App() {
   return (
-    <>
+    <div className="App">
       <Navbar />
-      <Hero />
-      <Notices />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Hero />
+            <Notices />
+          </>
+        } />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/activity/:domainId" element={<ActivityPage />} />
+      </Routes>
       <Footer />
-    </>
+    </div>
   );
 }
